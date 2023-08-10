4d6d29629bb70895c17363b57bf7e9f9752c2083
CardyBangz <133674601+CardyBangz@users.noreply.github.com>
2023-08-10 02:30:04 -0400

    Add files via upload

---
+++ Destructuring Exercise.js
@@ -0,0 +1,22 @@
+console.log(numPlanets);//8
+console.log(yearNeptuneDiscovered);//1846
+===========================================
+console.log(discoveryYears);//1846,1659
+===========================================
+"Your name is Alejandro and you like purple"
+"Your name is Melissa and you like green"
+"Your name is undefined and you like green"
+===========================================
+"Maya"
+"Marisa"
+"Chi"
+===========================================
+"Raindrops on roses"
+"Whiskers on kittens"
+("Bright copper kettles, Warm woolen mittens, Brown paper packages tied up with strings")
+===========================================
+10,30,20
+===========================================
+const {a,b}= obj.numbers
+===========================================
+[arr[1],arr[2]=arr[2],arr[1]]
